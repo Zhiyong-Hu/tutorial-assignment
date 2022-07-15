d374ab3bbafc640349dd7c737bb5b6eb07c88a4f
zhiyonghu <zhiyong.hu@crypto.com>
2022-07-15 09:51:07 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-const PRIVATE_KEY = "78e562f34fabb4fa55c48f7f184bcd2c585d4eaacb53adb1e77c7df0d1e407e6";
-// const PRIVATE_KEY = "Your private key";
+const PRIVATE_KEY = "Your private key";
 module.exports = {
   solidity: "0.8.0",
   networks: {
