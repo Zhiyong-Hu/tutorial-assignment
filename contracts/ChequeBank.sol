eea74ccf713b3f3eb3245fc58ed250c184b2214a
zhiyonghu <zhiyong.hu@crypto.com>
2022-07-20 09:44:56 +0800

    update

--- contracts/ChequeBank.sol
+++ contracts/ChequeBank.sol
@@ -3,8 +3,20 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "hardhat/console.sol";
 
 contract ChequeBank {
+    enum Status {
+        unuse,
+        overing,
+        used
+    }
+
     mapping(address => uint256) balances;
-    mapping(address => mapping(bytes32 => bool)) redeemeds;
+    mapping(bytes32 => Info) cheques;
+
+    struct Info {
+        uint8 counter;
+        Status status;
+        address payee;
+    }
 
     struct ChequeInfo {
         uint256 amount;
@@ -35,6 +47,7 @@ contract ChequeBank {
     }
 
     function deposit() external payable {
+        require(msg.value >= 0);
         balances[msg.sender] += msg.value;
     }
 
@@ -52,41 +65,26 @@ contract ChequeBank {
 
     function redeem(Cheque memory chequeData) external {
         require(
-            !redeemeds[chequeData.chequeInfo.payer][
-                chequeData.chequeInfo.chequeId
-            ],
-            "The cheque had redeemed"
+            cheques[chequeData.chequeInfo.chequeId].status == Status.unuse,
+            "The cheque can't redeem"
         );
         require(
             balances[chequeData.chequeInfo.payer] >=
                 chequeData.chequeInfo.amount,
             "Not enough balances"
         );
-
-        //TODO Valid From/Thru
-
-        bytes32 hash = keccak256(
-            abi.encodePacked(
-                chequeData.chequeInfo.chequeId,
-                chequeData.chequeInfo.payer,
-                chequeData.chequeInfo.payee,
-                chequeData.chequeInfo.amount,
-                this,
-                chequeData.chequeInfo.validFrom,
-                chequeData.chequeInfo.validThru
-            )
+        require(
+            block.number >= chequeData.chequeInfo.validFrom &&
+                block.number <= chequeData.chequeInfo.validThru,
+            "The cheque can't redeem"
         );
-        bytes32 message = ECDSA.toEthSignedMessageHash(hash);
 
         require(
-            ECDSA.recover(message, chequeData.sig) ==
-                chequeData.chequeInfo.payer,
+            recover(chequeData, address(this)) == chequeData.chequeInfo.payer,
             "The cheque invalid"
         );
 
-        redeemeds[chequeData.chequeInfo.payer][
-            chequeData.chequeInfo.chequeId
-        ] = true;
+        cheques[chequeData.chequeInfo.chequeId].status == Status.used;
         balances[chequeData.chequeInfo.payer] -= chequeData.chequeInfo.amount;
 
         payable(chequeData.chequeInfo.payee).transfer(
@@ -95,16 +93,44 @@ contract ChequeBank {
     }
 
     function revoke(bytes32 chequeId) external {
-        require(!redeemeds[msg.sender][chequeId], "The cheque had redeemed");
-        redeemeds[msg.sender][chequeId] = true;
+        require(
+            cheques[chequeId].status == Status.unuse,
+            "The cheque had redeemed"
+        );
+        cheques[chequeId].status == Status.used;
     }
 
-    function notifySignOver(SignOver memory signOverData) external {}
+    function notifySignOver(SignOver memory signOverData) external {
+        require(
+            cheques[signOverData.signOverInfo.chequeId].status != Status.used,
+            "The cheque had redeemed"
+        );
+        require(
+            recover(signOverData) == signOverData.signOverInfo.oldPayee,
+            "The cheque invalid"
+        );
+        cheques[signOverData.signOverInfo.chequeId].payee = signOverData
+            .signOverInfo
+            .newPayee;
+        cheques[signOverData.signOverInfo.chequeId].status = Status.overing;
+    }
 
     function redeemSignOver(
         Cheque memory chequeData,
         SignOver[] memory signOverData
-    ) external {}
+    ) external {
+        require(
+            recover(chequeData, address(this)) == chequeData.chequeInfo.payer,
+            "The cheque invalid"
+        );
+        for (uint256 i = 0; i < signOverData.length; i++) {
+            require(
+                recover(signOverData[i]) ==
+                    signOverData[i].signOverInfo.oldPayee,
+                "The cheque invalid"
+            );
+        }
+    }
 
     function isChequeValid(
         address payee,
@@ -113,9 +139,7 @@ contract ChequeBank {
     ) public view returns (bool) {
         require(chequeData.chequeInfo.payee == payee);
         require(
-            !redeemeds[chequeData.chequeInfo.payer][
-                chequeData.chequeInfo.chequeId
-            ],
+            cheques[chequeData.chequeInfo.chequeId].status != Status.used,
             "The cheque had redeemed"
         );
         require(
@@ -123,20 +147,55 @@ contract ChequeBank {
                 chequeData.chequeInfo.amount,
             "Not enough balances"
         );
+        if (recover(chequeData, address(this)) != chequeData.chequeInfo.payer) {
+            return false;
+        }
+        for (uint256 i = 0; i < signOverData.length; i++) {
+            if (
+                recover(signOverData[i]) !=
+                signOverData[i].signOverInfo.oldPayee
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function recover(Cheque memory chequeData, address addr)
+        internal
+        pure
+        returns (address)
+    {
         bytes32 hash = keccak256(
             abi.encodePacked(
                 chequeData.chequeInfo.chequeId,
                 chequeData.chequeInfo.payer,
                 chequeData.chequeInfo.payee,
                 chequeData.chequeInfo.amount,
-                this,
+                addr,
                 chequeData.chequeInfo.validFrom,
                 chequeData.chequeInfo.validThru
             )
         );
         bytes32 message = ECDSA.toEthSignedMessageHash(hash);
-        return
-            ECDSA.recover(message, chequeData.sig) ==
-            chequeData.chequeInfo.payer;
+        return ECDSA.recover(message, chequeData.sig);
+    }
+
+    function recover(SignOver memory signOverData)
+        internal
+        pure
+        returns (address)
+    {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes4(0xFFFFDEAD),
+                signOverData.signOverInfo.counter,
+                signOverData.signOverInfo.chequeId,
+                signOverData.signOverInfo.oldPayee,
+                signOverData.signOverInfo.newPayee
+            )
+        );
+        bytes32 message = ECDSA.toEthSignedMessageHash(hash);
+        return ECDSA.recover(message, signOverData.sig);
     }
 }
--- test/ChequeBank.js
+++ test/ChequeBank.js
@@ -5,7 +5,7 @@ const { Wallet } = require("ethers");
 describe("ChequeBank contract", function () {
 
   //bytes32
-  let chequeId = "1";
+  let chequeId = ethers.utils.formatBytes32String("1");
   //address
   let payer;
   //address
@@ -14,10 +14,6 @@ describe("ChequeBank contract", function () {
   let amount = ethers.utils.parseEther("1");
   //address
   let contractAddress;
-  //uint32
-  let validFrom = 0;
-  //uint32
-  let validThru = 0;
   let contract;
   let owner;
   let addr1;
@@ -58,41 +54,47 @@ describe("ChequeBank contract", function () {
     it("", async function () {
       let chequeInfo = {
         amount: amount,
-        chequeId: ethers.utils.formatBytes32String(chequeId),
-        validFrom: validFrom,
-        validThru: validThru,
+        chequeId: chequeId,
+        validFrom: 0,
+        validThru: 0,
         payee: payee,
         payer: payer
       };
-      let message = ethers.utils.solidityPack(["bytes32", "address", "address", "uint256", "address", "uint32", "uint32"], [ethers.utils.formatBytes32String(chequeId), payer, payee, amount, contractAddress, validFrom, validThru]);
+      let message = ethers.utils.solidityPack(["bytes32", "address", "address", "uint256", "address", "uint32", "uint32"], [chequeInfo.chequeId, chequeInfo.payer, chequeInfo.payee, chequeInfo.amount, contractAddress, chequeInfo.validFrom, chequeInfo.validThru]);
       let hash = ethers.utils.solidityKeccak256(["bytes"], [message]);
       let sig = await owner.signMessage(ethers.utils.arrayify(hash));
       let cheque = {
         chequeInfo: chequeInfo,
         sig: sig
       };
-      let counter = 1;
-      let signOverInfo = [
+
+      let signOverInfo = {
+        counter: 1,
+        chequeId: chequeId,
+        oldPayee: payee,
+        newPayee: addr2.address
+      };
+      let over_message = ethers.utils.solidityPack(["bytes4", "uint8", "bytes32", "address", "address"], [0xFFFFDEAD, signOverInfo.counter, signOverInfo.chequeId, signOverInfo.oldPayee, signOverInfo.newPayee]);
+      let over_hash = ethers.utils.solidityKeccak256(["bytes"], [over_message]);
+      let over_sig = await addr1.signMessage(ethers.utils.arrayify(over_hash));
+      let signOver = [
         {
-          counter: counter,
-          chequeId: ethers.utils.formatBytes32String(chequeId),
-          oldPayee: payee,
-          newPayee: addr2.address
+          signOverInfo: signOverInfo,
+          sig: over_sig
         }
       ];
-      let over_message = ethers.utils.solidityPack(["bytes", "uint8", "bytes32", "address", "address"], [0xFFFFDEAD, counter, ethers.utils.formatBytes32String(chequeId), payee, addr2.address]);
-      let over_hash = ethers.utils.solidityKeccak256(["bytes"], [over_message]);
-      let over_sig = await owner.signMessage(ethers.utils.arrayify(over_hash));
-      let signOver = {
-        signOverInfo: signOverInfo,
-        sig: over_sig
-      };
 
       await expect(contract.isChequeValid(payee, cheque, signOver)).to.be.revertedWith("Not enough balances");
       await contract.deposit({ value: amount })
       expect(await contract.isChequeValid(payee, cheque, signOver)).to.equal(true);
-      await contract.revoke(ethers.utils.formatBytes32String(chequeId))
+      await contract.revoke(chequeId)
       await expect(contract.isChequeValid(payee, cheque, signOver)).to.be.revertedWith("The cheque had redeemed");
+    });
+  });
+
+  describe("redeem", function () {
+    it("", async function () {
+
     });
   });
 
